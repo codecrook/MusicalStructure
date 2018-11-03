da95d81fe4ac23bed2ccf3360fff96608e1522d8
codecrook <hello@codecrook.io>
2018-11-03 19:45:47 +0530

    Dedigned the starting screen of the app
    What's Done:
    
    - Designed the main activity i.e the starting screen of the app
    - Added OnClickListener to `start music player` button. It navigates to PlaylistActivity that shows list of songs
    - Added custom icon for the app

--- app/src/main/java/io/codecrook/musicalstructure/MainActivity.java
+++ app/src/main/java/io/codecrook/musicalstructure/MainActivity.java
@@ -1,13 +1,28 @@
 package io.codecrook.musicalstructure;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startPlayerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startPlayerButton = findViewById(R.id.start_player_button);
+        startPlayerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startPlayer = new Intent(MainActivity.this, PlaylistActivity.class);
+                startActivity(startPlayer);
+                finish();
+            }
+        });
     }
 }
